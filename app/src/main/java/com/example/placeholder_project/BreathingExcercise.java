f150d77a0c2247867e5215a5b7782d1a50791fd9
helmiinaha <helmiina.halonen@metropolia.fi>
2022-03-02 13:02:09 +0200

    BreathingExercise

--- app/src/main/java/com/example/placeholder_project/BreathingExcercise.java
+++ app/src/main/java/com/example/placeholder_project/BreathingExcercise.java
@@ -1,14 +1,42 @@
 package com.example.placeholder_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BreathingExcercise extends AppCompatActivity {
+
+    public int counter;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breathing_excercise);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView8);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(180000,1000) {
+                    @Override
+                    public void onTick(long l) {
+                    textView.setText(String.valueOf(counter));
+                    counter++;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                    textView.setText("YOU DID A GREAT JOB!");
+                    }
+
+                }.start();
+            }
+        });
     }
 }
