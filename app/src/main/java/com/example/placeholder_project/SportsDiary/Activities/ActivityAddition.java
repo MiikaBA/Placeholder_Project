3bd63b0aee1523bd4bca0fd626ca6e9f2a3dad41
MiikaBA <miika.auvinen@metropolia.fi>
2022-03-08 12:44:46 +0200

    Tarkistetaan käyttäjän syöttämät tiedot.

--- app/src/main/java/com/example/placeholder_project/SportsDiary/Activities/ActivityAddition.java
+++ app/src/main/java/com/example/placeholder_project/SportsDiary/Activities/ActivityAddition.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.placeholder_project.R;
 import com.example.placeholder_project.SportsDiary.Classes.ActivitySingleton;
@@ -27,15 +28,33 @@ public class ActivityAddition extends AppCompatActivity {
         if(v == findViewById(R.id.creationButton)){
             EditText textToSave = findViewById(R.id.typeET);
             String type = textToSave.getText().toString();
+            if (type.matches("")) {
+                Toast.makeText(this, "You did not enter a type", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
             textToSave = findViewById(R.id.timeET);
+            String timeS = textToSave.getText().toString();
+            if (timeS.matches("")) {
+                Toast.makeText(this, "You did not enter a duration", Toast.LENGTH_SHORT).show();
+                return;
+            }
             int time = Integer.parseInt(textToSave.getText().toString());
 
             textToSave = findViewById(R.id.calsET);
+            String calories = textToSave.getText().toString();
+            if (calories.matches("")) {
+                Toast.makeText(this, "You did not enter burnt calories", Toast.LENGTH_SHORT).show();
+                return;
+            }
             int cals = Integer.parseInt(textToSave.getText().toString());
 
             textToSave = findViewById(R.id.descET);
             String desc = textToSave.getText().toString();
+            if (desc.matches("")) {
+                Toast.makeText(this, "You did not enter a description", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
             ActivitySingleton.getInstance().addActivity(type, time, cals, desc);
 
